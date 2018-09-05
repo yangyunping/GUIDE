e4f2b68414709dff0eb2a1767e92acdfa04d695e
emewe103 <2193227466@qq.com>
2018-09-05 18:01:53 +0800

    11

--- UI/FrmEmployee.designer.cs
+++ UI/FrmEmployee.designer.cs
@@ -28,11 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.txtAge = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
@@ -45,31 +42,18 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtName = new System.Windows.Forms.TextBox();
             this.lnl = new System.Windows.Forms.Label();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.twPower = new System.Windows.Forms.TreeView();
-            this.panel1.SuspendLayout();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.Transparent;
-            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.panel1.Controls.Add(this.btnClose);
-            this.panel1.Controls.Add(this.btnSave);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 486);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(558, 80);
-            this.panel1.TabIndex = 1;
             // 
             // btnClose
             // 
             this.btnClose.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.btnClose.Font = new System.Drawing.Font("微软雅黑", 9.5F);
-            this.btnClose.Location = new System.Drawing.Point(109, 23);
+            this.btnClose.Location = new System.Drawing.Point(91, 276);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(87, 40);
             this.btnClose.TabIndex = 33;
@@ -81,7 +65,7 @@
             // 
             this.btnSave.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.btnSave.Font = new System.Drawing.Font("微软雅黑", 9.5F);
-            this.btnSave.Location = new System.Drawing.Point(296, 23);
+            this.btnSave.Location = new System.Drawing.Point(278, 276);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(88, 40);
             this.btnSave.TabIndex = 32;
@@ -89,43 +73,9 @@
             this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(558, 486);
-            this.tabControl1.TabIndex = 2;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
-            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.tabPage1.Controls.Add(this.txtAge);
-            this.tabPage1.Controls.Add(this.label6);
-            this.tabPage1.Controls.Add(this.txtPassword);
-            this.tabPage1.Controls.Add(this.label5);
-            this.tabPage1.Controls.Add(this.cmbGender);
-            this.tabPage1.Controls.Add(this.label4);
-            this.tabPage1.Controls.Add(this.txtPhoneNum);
-            this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Controls.Add(this.txtID);
-            this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Controls.Add(this.txtName);
-            this.tabPage1.Controls.Add(this.lnl);
-            this.tabPage1.Location = new System.Drawing.Point(4, 28);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(550, 454);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "员工基本信息";
-            // 
             // txtAge
             // 
-            this.txtAge.Location = new System.Drawing.Point(95, 299);
+            this.txtAge.Location = new System.Drawing.Point(79, 189);
             this.txtAge.Name = "txtAge";
             this.txtAge.Size = new System.Drawing.Size(121, 25);
             this.txtAge.TabIndex = 31;
@@ -133,7 +83,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(54, 302);
+            this.label6.Location = new System.Drawing.Point(38, 192);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(37, 20);
             this.label6.TabIndex = 30;
@@ -141,7 +91,7 @@
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(337, 182);
+            this.txtPassword.Location = new System.Drawing.Point(321, 113);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.ReadOnly = true;
@@ -152,7 +102,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(266, 184);
+            this.label5.Location = new System.Drawing.Point(250, 115);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(65, 20);
             this.label5.TabIndex = 26;
@@ -166,7 +116,7 @@
             this.cmbGender.Items.AddRange(new object[] {
             "男",
             "女"});
-            this.cmbGender.Location = new System.Drawing.Point(95, 181);
+            this.cmbGender.Location = new System.Drawing.Point(79, 112);
             this.cmbGender.Name = "cmbGender";
             this.cmbGender.Size = new System.Drawing.Size(121, 27);
             this.cmbGender.TabIndex = 25;
@@ -174,7 +124,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(54, 184);
+            this.label4.Location = new System.Drawing.Point(38, 115);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(37, 20);
             this.label4.TabIndex = 24;
@@ -182,7 +132,7 @@
             // 
             // txtPhoneNum
             // 
-            this.txtPhoneNum.Location = new System.Drawing.Point(337, 296);
+            this.txtPhoneNum.Location = new System.Drawing.Point(321, 186);
             this.txtPhoneNum.Name = "txtPhoneNum";
             this.txtPhoneNum.Size = new System.Drawing.Size(138, 25);
             this.txtPhoneNum.TabIndex = 21;
@@ -190,7 +140,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(266, 299);
+            this.label2.Location = new System.Drawing.Point(250, 189);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 20);
             this.label2.TabIndex = 20;
@@ -198,7 +148,7 @@
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(337, 67);
+            this.txtID.Location = new System.Drawing.Point(321, 39);
             this.txtID.Name = "txtID";
             this.txtID.Size = new System.Drawing.Size(138, 25);
             this.txtID.TabIndex = 19;
@@ -206,7 +156,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(266, 70);
+            this.label1.Location = new System.Drawing.Point(250, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 20);
             this.label1.TabIndex = 18;
@@ -214,7 +164,7 @@
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(95, 67);
+            this.txtName.Location = new System.Drawing.Point(79, 39);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(121, 25);
             this.txtName.TabIndex = 17;
@@ -222,23 +172,11 @@
             // lnl
             // 
             this.lnl.AutoSize = true;
-            this.lnl.Location = new System.Drawing.Point(54, 70);
+            this.lnl.Location = new System.Drawing.Point(38, 42);
             this.lnl.Name = "lnl";
             this.lnl.Size = new System.Drawing.Size(37, 20);
             this.lnl.TabIndex = 16;
             this.lnl.Text = "姓名";
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.tabPage2.Controls.Add(this.twPower);
-            this.tabPage2.Location = new System.Drawing.Point(4, 28);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(550, 454);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "员工权限";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // twPower
             // 
@@ -246,42 +184,72 @@
             this.twPower.CheckBoxes = true;
             this.twPower.Dock = System.Windows.Forms.DockStyle.Fill;
             this.twPower.Indent = 21;
-            this.twPower.Location = new System.Drawing.Point(3, 3);
+            this.twPower.Location = new System.Drawing.Point(3, 21);
             this.twPower.Name = "twPower";
-            this.twPower.Size = new System.Drawing.Size(544, 448);
+            this.twPower.Size = new System.Drawing.Size(615, 541);
             this.twPower.TabIndex = 0;
             this.twPower.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.twPower_AfterCheck);
             this.twPower.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.twPower_AfterSelect);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClose);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.lnl);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtAge);
+            this.groupBox1.Controls.Add(this.txtPhoneNum);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbGender);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(518, 565);
+            this.groupBox1.TabIndex = 32;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "人员信息";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.twPower);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(518, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(621, 565);
+            this.groupBox2.TabIndex = 33;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "人员权限";
             // 
             // FrmEmployee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 566);
-            this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1139, 565);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("微软雅黑", 10F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmEmployee";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "员工信息";
-            this.panel1.ResumeLayout(false);
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
-            this.tabPage2.ResumeLayout(false);
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TextBox txtAge;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtPassword;
@@ -294,7 +262,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Label lnl;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TreeView twPower;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
